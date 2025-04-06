b99652c4aa03d3a256c210bbfe0b8c28734f02a4
Noor Rifat Aronnya <147256167+Aronnya-Rifat@users.noreply.github.com>
2025-04-06 09:33:41 +0600

    Update main.py

--- main.py
+++ main.py
@@ -456,7 +456,7 @@ def sync_redfin_with_google_sheet(
 
         print(f"❌ Google Sheets API error during update: {e}")
         if "exceeds grid limits" in str(e):
-            script_url = "https://script.google.com/macros/s/AKfycbxrDyHZOFKbnXeG8FdzjQytFBY-4DTNQTwlb9R3sOv_CAibPJwX6-2o2k4WXbSzrbPh/exec"
+            script_url = "https://script.google.com/macros/s/AKfycbzPLRnMgNisHHS3eaaGth8Morb-qEU-Y44dZVLdl384BhVah-r4-rpdBW3eGftp28dE/exec"
 
             response = requests.get(script_url)
 
@@ -491,7 +491,7 @@ def delete_all_csv():
 def main():
     SERVICE_ACCOUNT_FILE = json.loads(os.environ["GOOGLE_CREDENTIALS_FILE"])
     SCOPES = ['https://www.googleapis.com/auth/drive.file']
-    script_url = "https://script.google.com/macros/s/AKfycbxrDyHZOFKbnXeG8FdzjQytFBY-4DTNQTwlb9R3sOv_CAibPJwX6-2o2k4WXbSzrbPh/exec"
+    script_url = "https://script.google.com/macros/s/AKfycbzPLRnMgNisHHS3eaaGth8Morb-qEU-Y44dZVLdl384BhVah-r4-rpdBW3eGftp28dE/exec"
 
 
     folder_path = r"/app"
